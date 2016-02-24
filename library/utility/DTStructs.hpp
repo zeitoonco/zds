398fc485aca2d4e500b4b502d9401b45c3938eff
ajl <ajl@zeitoon.co>
2016-02-24 09:55:36 +0330

    DSBoolean & DSInstallInfo added

--- library/utility/DTStructs.hpp
+++ library/utility/DTStructs.hpp
@@ -11,12 +11,12 @@
 #include "datatypes/dtsingletypes.hpp"
 #include "datatypes/DTTableString.hpp"
 
-namespace zeitoon {
-namespace datatypes {
+namespace zeitoon { namespace datatypes {
 
-class DSString: public DTStruct {
+class DSString : public DTStruct {
 public:
-	DTString value = { "value" };
+	DTString value = {"value"};
+
 	DSString() :
 			DTStruct("DSString") {
 		this->list.push_back(&value);
@@ -25,17 +25,20 @@ public:
 	size_t getVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersionWOConversation() {
 		return 0;
 	}
 };
 
-class DSInteger: public DTStruct {
+class DSInteger : public DTStruct {
 public:
-	DTInteger<int> value = { "value" };
+	DTInteger<int> value = {"value"};
+
 	DSInteger() :
 			DTStruct("DSInteger") {
 		this->list.push_back(&value);
@@ -44,17 +47,20 @@ public:
 	size_t getVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersionWOConversation() {
 		return 0;
 	}
 };
 
-class DSFloat: public DTStruct {
+class DSFloat : public DTStruct {
 public:
-	DTFloat<float> value = { "value" };
+	DTFloat<float> value = {"value"};
+
 	DSFloat() :
 			DTStruct("DSFloat") {
 		this->list.push_back(&value);
@@ -63,17 +69,42 @@ public:
 	size_t getVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersionWOConversation() {
 		return 0;
 	}
 };
 
-class DSDBTable: public DTStruct {
+class DSBoolean : public DTStruct {
 public:
-	DTTableString value = { "value" };
+	DTBoolean value = {"value"};
+
+	DSBoolean() :
+			DTStruct("DSBoolean") {
+		this->list.push_back(&value);
+	}
+
+	size_t getVersion() {
+		return 0;
+	}
+
+	size_t getMinSupportedVersion() {
+		return 0;
+	}
+
+	size_t getMinSupportedVersionWOConversation() {
+		return 0;
+	}
+};
+
+class DSDBTable : public DTStruct {
+public:
+	DTTableString value = {"value"};
+
 	DSDBTable() :
 			DTStruct("DSDBTable") {
 		this->list.push_back(&value);
@@ -82,15 +113,145 @@ public:
 	size_t getVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersion() {
 		return 0;
 	}
+
 	size_t getMinSupportedVersionWOConversation() {
 		return 0;
 	}
 };
 
 
-}
-}
+class DSInstallInfo : public DTStruct {
+public:
+	class DSCommandDetail : public DTStruct {
+	public:
+		DTString name = {"name"};
+		DTString inputDatatype = {"inputDatatype"};
+		DTInteger inputDatatypeVersion = {"inputDatatypeVersion"};
+		DTString outputDatatype = {"outputDatatype"};
+		DTInteger outputDatatypeVersion = {"inputDatatypeVersion"};
+
+		DSCommandDetail(string iname, string inputDT, int inputDTVer, string outputDT, int outputDTVer) {
+			name = iname;
+			inputDatatype = inputDT;
+			inputDatatypeVersion = inputDTVer;
+			outputDatatype = outputDT;
+			outputDatatypeVersion = outputDTVer;
+			this->list.push_back(name);
+			this->list.push_back(inputDatatype);
+			this->list.push_back(inputDatatypeVersion);
+			this->list.push_back(outputDatatype);
+			this->list.push_back(outputDatatypeVersion);
+		}
+	};
+
+	class DSEventDetail : public DTStruct {
+	public:
+		DTString name = {"name"};
+		DTString datatype = {"datatype"};
+		DTInteger datatypeVersion = {"datatypeVersion"};
+
+		DSEventDetail(string iname, string DT, int DTVer) {
+			name = iname;
+			datatype = DT;
+			datatypeVersion = DTVer;
+			this->list.push_back(name);
+			this->list.push_back(datatype);
+			this->list.push_back(datatypeVersion);
+		}
+	};
+
+	class DSHookDetail : public DTStruct {
+	public:
+		DTString name = {"name"};
+		DTString datatype = {"datatype"};
+		DTInteger datatypeVersion = {"datatypeVersion"};
+
+		DSHookDetail(string iname, string DT, int DTVer) {
+			name = iname;
+			datatype = DT;
+			datatypeVersion = DTVer;
+			this->list.push_back(name);
+			this->list.push_back(datatype);
+			this->list.push_back(datatypeVersion);
+		}
+	};
+
+	class DSInstallInfoCommandCallDetail : public DTStruct {
+	public:
+		DTString name = {"name"};
+		DTInteger inputDatatypeVersion = {"inputDatatypeVersion"};
+		DTInteger outputDatatypeVersion = {"outputDatatypeVersion"};
+
+		DSCommandCallDetail(string
+		iname,
+		int inDTVer,
+		int outDTVer
+		) {
+			name = iname;
+			inputDatatypeVersion = inDTVer;
+			outputDatatypeVersion = outDTVer;
+			this->list.push_back(name);
+			this->list.push_back(inputDatatypeVersion);
+			this->list.push_back(outputDatatypeVersion);
+		}
+	};
+
+	class DSInstallInfoRequirementDetail : public DTStruct {
+	public:
+		DTString name = {"name"};
+		DTInteger version = {"version"};
+
+		DSInstallInfoRequirementDetail(string iname, int iVer) {
+			name = iname;
+			version = iVer;
+			this->list.push_back(name);
+			this->list.push_back(version);
+		}
+	};
+
+	class DSInstallInfoDatatypesDetail : public DTStruct {
+	public:
+		DTString name = {"name"};
+		DTInteger version = {"version"};
+
+		DSInstallInfoDatatypesDetail(string iname, int iVer) {
+			name = iname;
+			version = iVer;
+			this->list.push_back(name);
+			this->list.push_back(version);
+		}
+	};
+
+	DTString name = {"name"};
+	DTString title = {"title"};
+	DTInteger version = {"version"};
+	DTSet<DSCommandDetail> commands = {"commands"};
+	DTSet<DSEventDetail> events = {"events"};
+	DTSet<DSHookDetail> hooks = {"hooks"};
+	DTSet<DSInstallInfoCommandCallDetail> calls = {"calls"};
+	DTSet<DSInstallInfoRequirementDetail> requirements = {"requirements"};
+	DTSet<DSInstallInfoDatatypesDetail> datatypes = {"datatypes"};
+
+	DSInstallInfo() : DTStruct("installInfo") {
+
+	}
+
+	size_t getVersion() {
+		return 1;
+	}
+
+	size_t getMinSupportedVersion() {
+		return 1;
+	}
+
+	size_t getMinSupportedVersionWOConversation() {
+		return 1;
+	}
+};
+
+}}
 #endif /* DTSTRUCTS_HPP_ */

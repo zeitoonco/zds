2ebd02f3750f2940cb18fb2afc7142e08550b3e8
Inf <sar.navidi@gmail.com>
2016-04-03 11:39:52 +0430

    Config Management added, PGdatabasea and UserMgnt now use configuration manager

--- executables/PGdatabase/PGConfig.hpp
+++ executables/PGdatabase/PGConfig.hpp
@@ -15,11 +15,21 @@ public:
 	DTString serviceID = {"serviceID"};
 	DTString adminUsrPGDB = {"adminUsrPGDB"};
 	DTString adminPwPGDB = {"adminPwPGDB"};
+	DTString serverIP = {"serverIP"};
+	DTString serverPort = {"serverPort"};
+	DTString DBserverAddr = {"DBserverAddr"};
+	DTString DBserverPort = {"DBserverPort"};
+	DTString PGDBName = {"PGDBName"};
 
 	PGConfig() : ConfigManager("PGDatabase", "PGConfig") {
 		this->add(&serviceID);
 		this->add(&adminUsrPGDB);
 		this->add(&adminPwPGDB);
+		this->add(&serverIP);
+		this->add(&serverPort);
+		this->add(&DBserverAddr);
+		this->add(&DBserverPort);
+		this->add(&PGDBName);
 	}
 
 	~PGConfig() {

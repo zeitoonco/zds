50f1aab317a29b5bfd1f673cf83342ae85196668
Inf <sar.navidi@gmail.com>
2016-04-06 14:08:52 +0430

    PGDB & UserMgnt |-> main updated

--- executables/PGdatabase/main.cpp
+++ executables/PGdatabase/main.cpp
@@ -9,46 +9,48 @@
 using namespace zeitoon::pgdatabase;
 
 int main(int argc, char *argv[]) {// 1:Server IP  2:Server Port  3:
+	PGconfiguration.load();
+	std::string serverIP = "", serverPort = "", UsrPGDB = "", PwPGDB = "", DBAddr = "", DBPort = "", DBName = "";
 	if (argc != 8) {
 		std::cout << "Invalid number of arguments provided" << std::endl;
 		std::cout << "Required arguments: ServerIP ServerPort DBUsername DBPassword DBAddress DBPort DBName" <<
 		std::endl;
 		std::cout << "Trying to load from configuration" << std::endl;
-		try {
-			PGconfiguration.load();
-			PGmediator pg(PGconfiguration.adminUsrPGDB.getValue(), PGconfiguration.adminPwPGDB.getValue(),
-			              PGconfiguration.DBserverAddr.getValue(), std::stoi(PGconfiguration.DBserverPort.getValue()),
-			              PGconfiguration.PGDBName.getValue());
-			pg.connect(PGconfiguration.serverIP.getValue(), std::stoi(PGconfiguration.serverPort.getValue()));
-			pg.sm.joinNet();
-		} catch (exceptionEx *err) {
-			std::cerr << "ERROR:\n" << err->what();
-			return -1;
-		} catch (exception &err) {
-			std::cerr << "UnknownERROR:\n" << err.what();
-		}
+		serverIP = PGconfiguration.serverIP.getValue();
+		serverPort = PGconfiguration.serverPort.getValue();
+		UsrPGDB = PGconfiguration.adminUsrPGDB.getValue();
+		PwPGDB = PGconfiguration.adminPwPGDB.getValue();
+		DBAddr = PGconfiguration.DBserverAddr.getValue();
+		DBPort = PGconfiguration.DBserverPort.getValue();
+		DBName = PGconfiguration.PGDBName.getValue();
 
 	}
 	else {
 		std::cout << "Server Addr:\t" << argv[1] << "\tPort:\t" << argv[2] << std::endl;
 		std::cout << "Database Addr:\t" << argv[5] << "\tPort:\t" << argv[6] << std::endl;
 		try {
-			PGconfiguration.serverIP = argv[1];
-			PGconfiguration.serverPort = argv[2];
-			PGconfiguration.DBserverAddr = argv[5];
-			PGconfiguration.DBserverPort = argv[6];
-			PGconfiguration.adminUsrPGDB = argv[3];
-			PGconfiguration.adminPwPGDB = argv[4];
-			PGconfiguration.PGDBName = argv[7];
-			PGconfiguration.save();
-			PGmediator pg(argv[3], argv[4], argv[5], std::atoi(argv[6]), argv[7]);
-			pg.connect(argv[1], std::atoi(argv[2]));
-			pg.sm.joinNet();
+			serverIP = argv[1];
+			serverPort = argv[2];
+			DBAddr = argv[5];
+			DBPort = argv[6];
+			UsrPGDB = argv[3];
+			PwPGDB = argv[4];
+			DBName = argv[7];
+
 		} catch (exceptionEx *err) {
 			std::cerr << "ERROR:\n" << err->what();
 		} catch (exception &err) {
 			std::cerr << "UnknownERROR:\n" << err.what();
 		}
+	}
+	try {
+		PGmediator pg(UsrPGDB, PwPGDB, DBAddr, std::stoi(DBPort), DBName);
+		pg.connect(serverIP, std::stoi(serverPort));
+		pg.sm.joinNet();
+	} catch (exceptionEx *err) {
+		std::cerr << "ERROR:\n" << err->what();
+	} catch (exception &err) {
+		std::cerr << "UnknownERROR:\n" << err.what();
 	}
 	return 0;
 }
--- executables/UserManagement/main.cpp
+++ executables/UserManagement/main.cpp
@@ -7,38 +7,33 @@ using namespace zeitoon::usermanagement;
 
 int main(int argc, char *argv[]) {
 	UmCHI Umediator;
+	UMconfig.load();
+	std::string serverIP = "", serverPort = "";
 	if (argc != 3) {
 		std::cout << "Invalid number of arguments provided" << std::endl;
 		std::cout << "Required arguments: ServerIP ServerPort" << std::endl;
 		std::cout << "Trying to load from configuration" << std::endl;
 		try {
-			UMconfig.load();
-			Umediator.connect(UMconfig.serverIP.getValue(), std::stoi(UMconfig.serverPort.getValue()));
-			std::cout << "Server Addr:\t" << UMconfig.serverIP.getValue() << "\tPort:\t" <<
-			UMconfig.serverPort.getValue() << std::endl;
-			Umediator.sm.joinNet();
+			serverIP = UMconfig.serverIP.getValue();
+			serverPort = UMconfig.serverPort.getValue();
 		} catch (exceptionEx *err) {
-			std::cerr << "ERROR:\n" << err->what() << std::endl;
-			return -1;
-		} catch (exception &err) {
-			std::cerr << "UnknownERROR:\n" << err.what();
+			std::cerr << "Error:\n" << err->what() << std::endl;
 		}
 	} else {
-		std::cout << "Server Addr:\t" << UMconfig.serverIP.getValue() << "\tPort:\t" <<
-		UMconfig.serverPort.getValue() <<
-		std::endl;
-
-		try {
-			UMconfig.serverIP = argv[1];
-			UMconfig.serverPort = argv[2];
-			UMconfig.save();
-			Umediator.connect(argv[1], std::stoi(argv[2]));
-			Umediator.sm.joinNet();
-		} catch (exceptionEx *err) {
-			std::cerr << "ERROR:\n" << err->what();
-		} catch (exception &err) {
-			std::cerr << "UnknownERROR:\n" << err.what();
-		}
+		serverIP = argv[1];
+		serverPort = argv[2];
 	}
+
+
+	try {
+		Umediator.connect(serverIP, std::stoi(serverPort));
+		std::cout << "Server Addr:\t" << serverIP << "\tPort:\t" << serverPort << std::endl;
+		Umediator.sm.joinNet();
+	} catch (exceptionEx *err) {
+		std::cerr << "ERROR:\n" << err->what();
+	} catch (exception &err) {
+		std::cerr << "UnknownERROR:\n" << err.what();
+	}
+
 
 }

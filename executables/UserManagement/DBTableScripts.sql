7503fd4042b38ba412937b8ae13e2e3d009eeed1
inf64x <sar.navidi@gmail.com>
2016-12-25 18:48:43 +0330

    UserManagement database scripts updated.
    Scripts for functions added, order of execution rearranged, function triggers and triggers added.

--- executables/UserManagement/DBTableScripts.sql
+++ executables/UserManagement/DBTableScripts.sql
@@ -1,6 +1,5 @@
 BEGIN;
-
-CREATE TABLE if not exists "UserManagement".permission
+CREATE TABLE if not exists  permission
 (
   id serial NOT NULL,
   parentid integer,
@@ -9,15 +8,13 @@ CREATE TABLE if not exists "UserManagement".permission
   description character varying(1024) DEFAULT NULL::character varying,
   CONSTRAINT permission_pkey PRIMARY KEY (id),
   CONSTRAINT permission_parentid_fkey FOREIGN KEY (parentid)
-      REFERENCES "UserManagement".permission (id) MATCH SIMPLE
+      REFERENCES  permission (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE RESTRICT,
   CONSTRAINT permission_name_key UNIQUE (name)
-)
-WITH (
-  OIDS=FALSE
 );
 
-CREATE TABLE if not exists "UserManagement".users
+
+CREATE TABLE if not exists  users
 (
  id serial NOT NULL,
   username character varying(32),
@@ -28,12 +25,10 @@ CREATE TABLE if not exists "UserManagement".users
   avatar text DEFAULT NULL,
   CONSTRAINT users_pkey PRIMARY KEY (id),
   CONSTRAINT users_username_key UNIQUE (username)
-)
-WITH (
-  OIDS=FALSE
 );
 
-CREATE TABLE if not exists "UserManagement".groups
+
+CREATE TABLE if not exists  groups
 (
    id serial NOT NULL,
   title character varying(64),
@@ -41,100 +36,256 @@ CREATE TABLE if not exists "UserManagement".groups
   description character varying(4096) DEFAULT NULL::character varying,
   CONSTRAINT groups_pkey PRIMARY KEY (id),
   CONSTRAINT groups_parentid_fkey FOREIGN KEY (parentid)
-      REFERENCES "UserManagement".groups (id) MATCH SIMPLE
+      REFERENCES  groups (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE RESTRICT
-)
-WITH (
-  OIDS=FALSE
 );
 
 
-CREATE TABLE if not exists "UserManagement".grouppermission
+CREATE TABLE if not exists  grouppermission
 (
-    permissionid integer NOT NULL,
+  permissionid integer NOT NULL,
   groupid integer NOT NULL,
   state integer DEFAULT 0,
   CONSTRAINT grouppermission_pkey PRIMARY KEY (permissionid, groupid),
-  CONSTRAINT grouppermission_groupid_fkey FOREIGN KEY (groupid)
-      REFERENCES "UserManagement".groups (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE RESTRICT,
-  CONSTRAINT grouppermission_permissionid_fkey FOREIGN KEY (permissionid)
-      REFERENCES "UserManagement".permission (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE RESTRICT
-)
-WITH (
-  OIDS=FALSE
+  CONSTRAINT "grouppermission_GID" FOREIGN KEY (groupid)
+      REFERENCES  groups (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT "grouppermission_PID" FOREIGN KEY (permissionid)
+      REFERENCES  permission (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE if not exists "UserManagement".servicepermission
+
+CREATE TABLE if not exists  servicepermission
 (
    id serial NOT NULL,
   service character varying(64),
   permissionid integer,
   CONSTRAINT servicepermission_pkey PRIMARY KEY (id),
   CONSTRAINT servicepermission_permissionid_fkey FOREIGN KEY (permissionid)
-      REFERENCES "UserManagement".permission (id) MATCH SIMPLE
+      REFERENCES  permission (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
-      )
-WITH (
-  OIDS=FALSE
-);
+      );
 
 
-CREATE TABLE if not exists "UserManagement".usergroup
+CREATE TABLE if not exists  usergroup
 (
   userid integer NOT NULL,
   groupid integer NOT NULL,
   CONSTRAINT usergroup_pkey PRIMARY KEY (userid, groupid),
   CONSTRAINT usergroup_groupid_fkey FOREIGN KEY (groupid)
-      REFERENCES "UserManagement".groups (id) MATCH SIMPLE
+      REFERENCES  groups (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE RESTRICT,
   CONSTRAINT usergroup_userid_fkey FOREIGN KEY (userid)
-      REFERENCES "UserManagement".users (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE RESTRICT
-
-)
-WITH (
-  OIDS=FALSE
+      REFERENCES  users (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
-
-CREATE TABLE if not exists "UserManagement".userpermission
+CREATE TABLE if not exists  userpermission
 (
  userid integer NOT NULL,
   permissionid integer NOT NULL,
   state integer DEFAULT 0,
   CONSTRAINT userpermission_pkey PRIMARY KEY (userid, permissionid),
   CONSTRAINT userpermission_permissionid_fkey FOREIGN KEY (permissionid)
-      REFERENCES "UserManagement".permission (id) MATCH SIMPLE
+      REFERENCES  permission (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE RESTRICT,
   CONSTRAINT userpermission_userid_fkey FOREIGN KEY (userid)
-      REFERENCES "UserManagement".users (id) MATCH SIMPLE
+      REFERENCES  users (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
-)
-WITH (
-  OIDS=FALSE
 );
 
 
-CREATE TABLE if not exists "UserManagement".contacts
+CREATE TABLE  contacts
 (
   userid integer NOT NULL,
   contactid integer NOT NULL,
   note character varying,
   CONSTRAINT contacts_pkey PRIMARY KEY (userid, contactid),
   CONSTRAINT contacts_contactid_fkey FOREIGN KEY (contactid)
-      REFERENCES "UserManagement".users (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE NO ACTION,
+      REFERENCES  users (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT contacts_userid_fkey FOREIGN KEY (userid)
-      REFERENCES "UserManagement".users (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
+      REFERENCES  users (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+
+--MASTER PERMISSION
 INSERT INTO permission(id,parentid,name,title) select 0, NULL,'_masterPermission','Permissions' WHERE NOT EXISTS (SELECT id FROM permission WHERE id=0);
 
+
+
+--TRIGGER FUNCTIONS
+CREATE OR REPLACE FUNCTION  userpermissiongt()
+  RETURNS trigger AS
+$BODY$ BEGIN
+	IF TG_OP='DELETE' THEN
+		IF OLD.userid=0 THEN
+			RAISE EXCEPTION USING MESSAGE='CANNOT REMOVE AN ADMIN PRIVILEGE';
+		END IF;
+		RETURN OLD;
+	END IF;
+	IF TG_OP='INSERT' THEN
+		IF NEW.userid=0 THEN
+		RAISE EXCEPTION USING MESSAGE='CANNOT CREATE AN ADMIN PRIVILEGE';
+		END IF;
+		RETURN NEW;
+	END IF;
+	IF TG_OP='UPDATE' THEN
+		IF OLD.userid=0 THEN
+			RAISE EXCEPTION USING MESSAGE='CANNOT MODIFY AN ADMIN PRIVILEGE';
+		END IF;
+		RETURN NEW;
+	END IF;
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE;
+-------------
+CREATE OR REPLACE FUNCTION  usersgt()
+  RETURNS trigger AS
+$BODY$
+
+BEGIN
+	IF TG_OP='DELETE' THEN
+		IF (OLD.id=0) THEN
+		RAISE EXCEPTION USING MESSAGE='CANNOT REMOVE ADMIN ACCOUNT';
+		END IF;
+		RETURN OLD;
+	END IF;
+
+
+	IF TG_OP='UPDATE' THEN
+		IF (OLD.id =0 OR OLD.username='admin') THEN
+			NEW.banned=FALSE;
+			NEW.username=OLD.username;
+		END IF;
+		NEW.id= OLD.id;
+		RETURN NEW;
+	END IF;
+
+
+RETURN NEW;
+END;$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+
+---TRIGGERS
+  CREATE TRIGGER userpermissiontrigger
+  BEFORE INSERT OR UPDATE OR DELETE
+  ON  userpermission
+  FOR EACH ROW
+  EXECUTE PROCEDURE  userpermissiongt();
+  ---------------
+  CREATE TRIGGER userstrigger
+  BEFORE INSERT OR UPDATE OR DELETE
+  ON  users
+  FOR EACH ROW
+  EXECUTE PROCEDURE  usersgt();
+  ------------------------------------------------------
+  ---FUNCTIONS
+  CREATE OR REPLACE FUNCTION  addusersusergroup(
+    IN uid integer,
+    IN p1 integer[])
+  RETURNS TABLE(userid integer, groupid integer, errormsg text, sqlstate text) AS
+$BODY$
+	DECLARE
+		i	integer;
+	BEGIN
+--FOREACH i IN ARRAY p1
+	      FOR i IN array_lower(p1, 1).. array_upper(p1, 1)
+	       loop
+	       BEGIN
+
+		     insert into usergroup(userid, groupid) values(uid,P1[i]);
+	       EXCEPTION
+
+		 WHEN  OTHERS THEN
+		  WHEN  OTHERS THEN
+			RETURN QUERY SELECT uid, P1[i], SQLERRM, SQLSTATE ;
+	       END;
+
+	       end loop;
+	END $BODY$
+  LANGUAGE plpgsql VOLATILE;
+  -----------------------
+  CREATE OR REPLACE FUNCTION  removeusersusergroup(
+    IN uid integer,
+    IN p1 integer[])
+  RETURNS TABLE(userid integer, groupid integer, errormsg text, sqlstate text) AS
+$BODY$
+	DECLARE
+		i	integer;
+	BEGIN
+RAISE NOTICE 'REMOVING GROUPS FOR USER: %', uid;
+	       FOR i IN 1 .. array_upper(P1, 1)
+	       loop
+	       BEGIN
+RAISE NOTICE 'REMOVING %',P1[i];
+		     DELETE FROM usergroup WHERE ("usergroup".userid =uid AND "usergroup".groupid=P1[i]);
+	       EXCEPTION
+
+		 WHEN  OTHERS THEN
+		  WHEN  OTHERS THEN
+			RETURN QUERY SELECT uid , P1[i], SQLERRM, SQLSTATE ;
+	       END;
+
+	       end loop;
+
+	END $BODY$
+  LANGUAGE plpgsql VOLATILE;
+  --------------------
+  CREATE OR REPLACE FUNCTION  setgrouppermission(
+    IN gid integer,
+    IN p1 integer[])
+  RETURNS TABLE(pid integer, pstate integer, errormsg text, sqlstate text) AS
+$BODY$
+	DECLARE
+		i	integer;
+	BEGIN
+	       FOR i IN 1 .. array_upper(P1, 1)
+	       loop
+	       BEGIN
+		     insert into grouppermission values(P1[i][1],GID,P1[i][2])
+		      ON CONFLICT (permissionid,groupid) DO UPDATE SET state=P1[i][2];
+	       EXCEPTION
+
+		 WHEN  OTHERS THEN
+		  WHEN  OTHERS THEN
+			RETURN QUERY SELECT P1[i][1] , P1[i][2], SQLERRM, SQLSTATE ;
+	       END;
+
+	       end loop;
+
+	END $BODY$
+  LANGUAGE plpgsql VOLATILE;
+  -------------------------
+  CREATE OR REPLACE FUNCTION  setuserpermission(
+    IN uid integer,
+    IN p1 integer[])
+  RETURNS TABLE(pid integer, pstate integer, errormsg text, sqlstate text) AS
+$BODY$
+	DECLARE
+		i integer;
+
+	BEGIN
+
+	       FOR i IN 1 .. array_upper(P1, 1)
+	       loop
+	       BEGIN
+
+		     insert into userpermission values(uid,P1[i][1],P1[i][2])
+		      ON CONFLICT (permissionid,userid) DO UPDATE SET state=P1[i][2];
+
+	       EXCEPTION
+
+		 WHEN  OTHERS THEN
+			RETURN QUERY SELECT P1[i][1] , P1[i][2], SQLERRM, SQLSTATE ;
+	       END;
+
+	       end loop;
+
+
+	END $BODY$
+  LANGUAGE plpgsql VOLATILE;
 END;
